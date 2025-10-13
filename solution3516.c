67ca667ef65eda684735dbbaec30f8eb05de490b
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-10-13 19:44:54 +0530

    Create solution3516.c

---
+++ solution3516.c
@@ -0,0 +1,13 @@
+int findClosest(int x, int y, int z) {
+    int per1 = abs(x-z) ;
+    int per2 = abs(y-z) ;
+    if (per1 < per2){
+        return 1;
+    }
+    else if (per1 > per2){
+        return 2;
+    }
+    else{
+        return 0;
+    }
+}
