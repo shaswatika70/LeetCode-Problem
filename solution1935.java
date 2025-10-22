fbe287b7a6345dbd7fe489e418ad81ee6d97f977
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-10-22 21:26:55 +0530

    Clean up comments in canBeTypedWords method
    Removed comments for cleaner code.

--- solution1935.java
+++ solution1935.java
@@ -1,6 +1,6 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        // Convert broken letters into a set for fast lookup
+        
         boolean[] broken = new boolean[26];
         for (char c : brokenLetters.toCharArray()) {
             broken[c - 'a'] = true;
@@ -9,7 +9,7 @@ class Solution {
         int count = 0;
         String[] words = text.split(" ");
 
-        // Check each word
+        
         for (String word : words) {
             boolean canType = true;
             for (char c : word.toCharArray()) {
