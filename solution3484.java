d46866b8f092fb1f4bf499d1705a3731cb03c245
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-10-26 20:38:35 +0530

    Implement Spreadsheet class with basic functionality

---
+++ solution3484.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Spreadsheet {
+    private Map<String, Integer> spreadsheet;
+
+    public Spreadsheet(int rows) {
+        spreadsheet = new HashMap<>();
+    }
+
+    public void setCell(String cell, int value) {
+        spreadsheet.put(cell, value);
+    }
+
+    public void resetCell(String cell) {
+        spreadsheet.put(cell, 0);
+    }
+
+    public int getValue(String formula) {
+        int plus = formula.indexOf('+');
+        String left = formula.substring(1, plus);
+        String right = formula.substring(plus + 1);
+        return getToken(left) + getToken(right);
+    }
+
+    private int getToken(String token) {
+        if (Character.isDigit(token.charAt(0))) {
+            return Integer.parseInt(token);
+        } else {
+            return spreadsheet.getOrDefault(token, 0);
+        }
+    }
+}
