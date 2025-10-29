5b765462588deb4164c0b665b30ab4d0560628c8
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-10-29 21:31:29 +0530

    Fix formatting of addTwoNumbers method

--- solution2.java
+++ solution2.java
@@ -1,10 +1,12 @@
 class Solution {
   
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+  {
         ListNode dummyHead = new ListNode(0);
         ListNode curr = dummyHead;
         int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
+        while (l1 != null || l2 != null || carry != 0) 
+        {
             int x = (l1 != null) ? l1.val : 0;
             int y = (l2 != null) ? l2.val : 0;
             int sum = carry + x + y;
