d50bb15c043daab8379260d972f153ffa7e99f58
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-10-29 21:30:55 +0530

    Remove outdated comment from addTwoNumbers method
    Removed outdated comment about the method.

--- solution2.java
+++ solution2.java
@@ -1,5 +1,5 @@
 class Solution {
-    // Add Two Numbers (Java improved)
+  
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode curr = dummyHead;
