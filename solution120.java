0e16292940dad45e411ba7f45e9fab07d20a176a
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-11-04 22:29:38 +0530

    Create solution120.java

---
+++ solution120.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        
+        // Start from second last row and move upward
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int curr = triangle.get(i).get(j);
+                int below = triangle.get(i + 1).get(j);
+                int belowRight = triangle.get(i + 1).get(j + 1);
+                triangle.get(i).set(j, curr + Math.min(below, belowRight));
+            }
+        }
+        
+        return triangle.get(0).get(0);
+    }
+}
