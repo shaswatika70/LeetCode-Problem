01ad4a0f8e35c143088ebe66d716092d3bbb43f8
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2025-12-17 20:40:46 +0530

    Implement findFinalValue method in Solution class

---
+++ solution2154.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        for (int num : nums) {
+            if (original == num) {
+                original *= 2;
+            }
+        }
+        return original;
+    }
+}
