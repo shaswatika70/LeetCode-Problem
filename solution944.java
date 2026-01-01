14b1fc1bb34d3b833f861992ac08386af66d21cc
Shaswatika Muduli <shaswatikamuduli77@gmail.com>
2026-01-01 11:45:38 +0530

    Create solution944.java

---
+++ solution944.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        int rows = strs.length;
+        int cols = strs[0].length();
+
+        
+        for (int j = 0; j < cols; j++) {
+         
+            for (int i = 1; i < rows; i++) {
+                if (strs[i].charAt(j) < strs[i - 1].charAt(j)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
